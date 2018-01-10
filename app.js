b0d9a588fa6346aeb49f1e903040f8b89c583d54
wudi3 <wudi3@staff.weibo.com>
2018-01-10 12:42:13 +0800

    修改bug

--- app.js
+++ app.js
@@ -8,38 +8,40 @@ const serve = require('koa-static');
 const path = require('path');
 const request = require('request-promise');
 
-router.get('/', ctx => {
-    ctx.render('public/index.html');
-});
-
-router.get('/api/get/weather', async ctx => {
-    const options = {
-        method: `get`
-        ,json: true
-        ,url: `http://www.sojson.com/open/api/weather/json.shtml`
-        ,qs: { 'city': `北京` }
-    };
-    let res = await request(options);
-    ctx.body = res;
-});
-
-router.get('/api/get/calendar', async ctx => {
-    const options = {
-        method: `get`
-        ,json: true
-        ,url: `http://www.sojson.com/open/api/lunar/json.shtml`
-    };
-    let res = await request(options);
-    ctx.body = res;
-});
-
-app.use(bodyParser());
-app.use(serve(path.join(__dirname, './public')));
-app.use(logger());
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
-
-app.listen(3000);
-console.log('Running site at: http://localhost:3000');
+module.exports = function() {
+    router.get('/', ctx => {
+        ctx.render('public/index.html');
+    });
+    
+    router.get('/api/get/weather', async ctx => {
+        const options = {
+            method: `get`
+            ,json: true
+            ,url: `http://www.sojson.com/open/api/weather/json.shtml`
+            ,qs: { 'city': `北京` }
+        };
+        let res = await request(options);
+        ctx.body = res;
+    });
+    
+    router.get('/api/get/calendar', async ctx => {
+        const options = {
+            method: `get`
+            ,json: true
+            ,url: `http://www.sojson.com/open/api/lunar/json.shtml`
+        };
+        let res = await request(options);
+        ctx.body = res;
+    });
+    
+    app.use(bodyParser());
+    app.use(serve(path.join(__dirname, './public')));
+    app.use(logger());
+    app
+      .use(router.routes())
+      .use(router.allowedMethods());
+    
+    app.listen(3000);
+    console.log('Running site at: http://localhost:3000');
+};
 
